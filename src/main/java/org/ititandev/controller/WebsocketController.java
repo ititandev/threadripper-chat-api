d8838540613f6a555604d91fcdf324ef5c4b78e8
ITitanDev <manhpcpro@gmail.com>
2018-10-20 13:56:15 +0700

    check server support websocket

--- src/main/java/org/ititandev/controller/WebsocketController.java
+++ src/main/java/org/ititandev/controller/WebsocketController.java
@@ -1,32 +1,28 @@
 package org.ititandev.controller;
 
 import org.ititandev.model.ChatMessage;
-import org.ititandev.model.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
-
 @Controller
 public class WebsocketController {
 
-    @MessageMapping("/chat.sendMessage")
-    @SendTo("/topic/public")
-    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
-    	System.out.println(chatMessage.getContent());
-        return chatMessage;
-    }
+	@MessageMapping("/chat.sendMessage")
+	@SendTo("/topic/public")
+	public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+		return chatMessage;
+	}
 
-    @MessageMapping("/chat.addUser")
-    @SendTo("/topic/public")
-    public ChatMessage addUser(@Payload ChatMessage chatMessage,
-                               SimpMessageHeaderAccessor headerAccessor) {
-        // Add username in web socket session
-        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
-        System.out.println("/chat.addUser  " + chatMessage.getSender());
-        return chatMessage;
-    }
-    
+	@MessageMapping("/chat.addUser")
+	@SendTo("/topic/public")
+	public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		// Add username in web socket session
+		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+		System.out.println("/chat.addUser  " + chatMessage.getSender());
+		return chatMessage;
+	}
+
 }
--- src/main/java/org/ititandev/model/ChatMessage.java
+++ src/main/java/org/ititandev/model/ChatMessage.java
@@ -7,6 +7,7 @@ public class ChatMessage {
     private String sender;
 
     public enum MessageType {
+    	CHAT,
         TEXT,
         JOIN,
         LEAVE,
