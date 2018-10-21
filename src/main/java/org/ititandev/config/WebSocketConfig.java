4e5e1a8b305d5c3c9a71dd1f9b6fa720503b23d3
ITitanDev <manhpcpro@gmail.com>
2018-10-22 04:42:13 +0700

    add RabbitMQ broker

--- src/main/java/org/ititandev/config/WebSocketConfig.java
+++ src/main/java/org/ititandev/config/WebSocketConfig.java
@@ -22,12 +22,21 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
 		registry.setApplicationDestinationPrefixes("/queue");
-		registry.enableSimpleBroker("/topic"); // Enables a simple in-memory broker
+//		registry.enableSimpleBroker("/topic"); // Enables a simple in-memory broker
 
 		/* Use this for enabling a Full featured broker like RabbitMQ
 		 * registry.enableStompBrokerRelay("/topic") .setRelayHost("localhost")
 		 * .setRelayPort(61613) .setClientLogin("guest") .setClientPasscode("guest");
 		 */
+	    
+		registry.enableStompBrokerRelay("/topic")
+		.setRelayHost("localhost")
+	    .setRelayPort(61613)
+	    .setClientLogin("guest")
+	    .setClientPasscode("guest");
+	    
+	    
+//	    http://localhost:15672/#/
 	}
 
 	@Override
